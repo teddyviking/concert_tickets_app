764806f7e441e112cd2575c0bbc3e805540c971a
Rafael Tejado <rtejado90@gmail.com>
2016-01-16 20:53:29 +0100

    First commit

---
+++ spec/models/cool_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe 'Patata', type: :model do
+  
+  describe 'patata' do
+    it 'has a valid factory' do
+      # expect(FactoryGirl.create(:patata)).to be_valid
+    end
+  end
+end
---
+++ spec/support/database_cleaner.rb
@@ -0,0 +1,13 @@
+RSpec.configure do |config|
+
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.after(:suite) do
+    DatabaseCleaner.clean_with(:truncation)
+  end
+  
+
+end
